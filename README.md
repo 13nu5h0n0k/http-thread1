f41ea792502e0a67e50e3ec847401e43988eeaa9
Lena <elenapogrebetskaya@gmail.com>
2015-11-19 00:13:37 +0300

    начало

---
+++ README.md
@@ -0,0 +1,1 @@
+# http-thread1
---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+int main()
+{
+    int port = 10000;
+    int server_socket, client_socket;    
+    struct sockaddr_in server_address, client_address;
+    socklen_t address_size;
+    char buffer[100];
+    int yes = 1;
+
+    server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+    memset(&server_address, 0, sizeof(struct sockaddr_in));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = 0;
+        
+    bind(server_socket, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+
+    listen(server_socket, 10);
+
+    address_size = sizeof(struct sockaddr_in);
+
+    while (1)
+    {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_address, &address_size);
+
+        if (recv(client_socket, buffer, 100, 0) != -1)
+        {
+            printf("client: %s", buffer);
+            send(client_socket, "hello\n", 6, 0);
+            close(client_socket);
+        }
+    }
+}
